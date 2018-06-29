77cccf7cb030c08c4b254497054770dfaa669bae
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-06-29 13:27:08 -0400

    Completed ForumList and ForumListItem add added ForumList to PageHome.

--- vueSchool/vueschool-forum/src/components/ForumList.vue
+++ vueSchool/vueschool-forum/src/components/ForumList.vue
@@ -2,52 +2,31 @@
     <div class="forum-list">
 
         <h2 class="list-title">
-            <a href="category.html">Feedback &amp; Information</a>
+            <a href="#">Forums</a>
         </h2>
-
-        <div class="forum-listing">
-            <div class="forum-details">
-                <a class="text-xlarge" href="forum.html">Announcements</a>
-                <p>Important announcements about the forum.</p>
-            </div>
-
-            <div class="threads-count">
-                <p><span class="count">1</span> thread</p>
-            </div>
-
-            <div class="last-thread">
-                <img class="avatar" src="https://firebasestorage.googleapis.com/v0/b/forum-2a982.appspot.com/o/images%2Favatars%2Frah?alt=media&amp;token=7ad21914-a4f4-4ad0-add6-17e6d3ae9679" alt="">
-                <div class="last-thread-details">
-                    <a href="thread.html">Post Reactions</a>
-                    <p class="text-xsmall">By <a href="profile.html">Rolf Haug</a>, a month ago</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="forum-listing">
-            <div class="forum-details">
-                <a href="forum.html" class="text-xlarge">Questions &amp; Feedback</a>
-                <p>Ask your questions and drop your feedback here</p>
-            </div>
-
-            <div class="threads-count">
-                <p class="count">73</p> threads
-            </div>
-
-            <div class="last-thread">
-                <img class="avatar" src="https://www.sideshowtoy.com/photo_9030371_thumb.jpg" alt="">
-                <div class="last-thread-details">
-                    <a href="thread.html">Where is the sign in button?</a>
-                    <p class="text-xsmall">By <a href="profile.html">Joseph Kerr</a>, three hours ago</p>
-                </div>
-            </div>
-        </div>
+        <forum-list-item
+                v-for="forum in forums"
+                :forum="forum"
+                :key="forum['.key']"
+        ></forum-list-item>
 
     </div>
 </template>
 
 <script>
-    export default {}
+    import ForumListItem from './ForumListItem'
+
+    export default {
+      components: {
+        ForumListItem
+      },
+      props: {
+        forums: {
+          required: true,
+          type: Array
+        }
+      }
+    }
 </script>
 
 <style></style>
--- vueSchool/vueschool-forum/src/components/ForumListItem.vue
+++ vueSchool/vueschool-forum/src/components/ForumListItem.vue
@@ -1,7 +1,44 @@
-<template></template>
+<template>
+    <div class="forum-listing">
+        <div class="forum-details">
+            <router-link class="text-xlarge"
+                         :to="{name: 'Forum', params: {id: forum['.key']}}"
+            >
+                {{forum.name}}
+            </router-link>
+            <p>{{forum.description}}</p>
+        </div>
+
+        <div class="threads-count">
+            <p class="count">{{threadsCount}}</p> {{threadsCount === 1 ? 'thread' : 'threads'}}
+        </div>
+
+        <div class="last-thread">
+            <!--img class="avatar" src="https://www.sideshowtoy.com/photo_9030371_thumb.jpg" alt="">
+            <div class="last-thread-details">
+                <a href="thread.html">Where is the sign in button?</a>
+                <p class="text-xsmall">By <a href="profile.html">Joseph Kerr</a>, three hours ago</p>
+            </div ToDo later -->
+        </div>
+    </div>
+</template>
 
 <script>
-    export default {}
+    export default {
+      props: {
+        forum: {
+          required: true,
+          type: Object
+        }
+      },
+      computed: {
+        threadsCount () {
+          return this.forum.threads
+            ? Object.values(this.forum.threads).length
+            : 0
+        }
+      }
+    }
 </script>
 
 <style></style>
