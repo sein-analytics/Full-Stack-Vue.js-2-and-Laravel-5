ad4dee7aed838c27b865b92e4a7aa0097e489e18
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-07-02 10:29:33 -0400

    implemented mockup for getting an authenticated user.

--- vueSchool/vueschool-forum/src/store/index.js
+++ vueSchool/vueschool-forum/src/store/index.js
@@ -5,7 +5,15 @@ import sourceData from '@/data'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: sourceData,
+  state: {
+    ...sourceData,
+    authId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+  },
+  getters: {
+    authUser (state) {
+      return state.users[state.authId]
+    }
+  },
   actions: {
     createPost (context, post) {
       const postId = 'greatPost' + Math.random()
