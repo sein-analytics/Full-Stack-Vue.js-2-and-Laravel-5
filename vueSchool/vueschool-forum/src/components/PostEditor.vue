3cca425a96be2fdc002b566c025a88bf5810e306
Samuel Belu-John <samuel.belujohn@seinanalytics.com>
2018-07-06 15:03:48 -0400

    Implemented post editing functionality, created updatePost action, implemented validator in PostEditor

--- vueSchool/vueschool-forum/src/components/PostEditor.vue
+++ vueSchool/vueschool-forum/src/components/PostEditor.vue
@@ -6,7 +6,8 @@
                 ></textarea>
         </div>
         <div class="form-actions">
-            <button class="btn-blue">Submit Post</button>
+            <button v-if="isUpdate" @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
+            <button class="btn-blue">{{isUpdate ? 'Update' : 'Submit post'}}</button>
         </div>
     </form>
 </template>
@@ -15,23 +16,69 @@
     export default {
       props: {
         threadId: {
-          required: true
+          required: false
+        },
+        post: {
+          type: Object,
+          validator: obj => {
+            const keyIsValid = typeof obj['.key'] === 'string'
+            const textIsValid = typeof obj.text === 'string'
+            const valid = keyIsValid && textIsValid
+            if (!keyIsValid) {
+              console.error('The post prop object mus include a .key attribute')
+            }
+            if (!textIsValid) {
+              console.error('The post prop object mus include a text attribute')
+            }
+            return valid
+          }
         }
       },
       data () {
         return {
-          text: ''
+          text: this.post ? this.post.text : ''
+        }
+      },
+      computed: {
+        // Here we convert a variable into boolean using the double exclamation mark
+        isUpdate () {
+          return !!this.post
         }
       },
       methods: {
         save () {
+          this.persist()
+            .then(post => {
+              this.$emit('save', {post})
+            })
+        },
+        cancel () {
+          this.$emit('cancel')
+        },
+        create () {
           const post = {
             text: this.text,
             threadId: this.threadId
           }
           this.text = ''
           this.$emit('save', {post})
-          this.$store.dispatch('createPost', post)
+          return this.$store.dispatch('createPost', post)
+            /* .then(post => {
+              this.$emit('save', {post})
+            }) */
+        },
+        update () {
+          const payload = {
+            id: this.post['.key'],
+            text: this.text
+          }
+          return this.$store.dispatch('updatePost', payload)
+            /* .then(post => {
+              this.$emit('save', {post})
+            }) */
+        },
+        persist () {
+          return this.update ? this.update() : this.create()
         }
       }
     }
--- vueSchool/vueschool-forum/src/components/PostListItem.vue
+++ vueSchool/vueschool-forum/src/components/PostListItem.vue
@@ -8,11 +8,23 @@
             <p class="desktop-only text-small">{{userPostCount}}</p>
         </div>
         <div class="post-content">
-            <div>
-                {{post.text}}
+            <!-- v-if on a template element serves as an invisible wrapper -->
+            <template v-if="!editing">
+                <div>
+                    {{post.text}}
+                </div>
+                <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change"><i class="fa fa-pencil"></i></a>
+            </template>
+            <div v-else>
+                <PostEditor
+                        :post="post"
+                        @save="editing = false"
+                        @cancel="editing = false"
+                />
             </div>
         </div>
         <div class="post-date text-faded">
+            <div v-if="post.edited" class="edition-info">edited</div>
             <AppDate :timestamp="post.publishedAt"/>
         </div>
     </div>
@@ -20,15 +32,21 @@
 
 <script>
     import {countObjectProperties} from '@/utils'
+    import PostEditor from './PostEditor'
 
     export default {
-      /* components: {
-        AppDate
-      }, */
+      components: {
+        PostEditor
+      },
       props: {
         post: {
           required: true,
           type: Object
+        }
+      },
+      data () {
+        return {
+          editing: false
         }
       },
       computed: {
